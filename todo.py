c90456caaedf537238aa67f70b14db741ef91d6d
Rohan Maligi <rmaligi@uci.edu>
2026-09-04 18:41:14 -0700

    switched over to the text file to make sure that I am saving the list instead of having to createa new file every single time

--- todo.py
+++ todo.py
@@ -13,21 +13,39 @@ while True:
     if choice == 1:
         task = input("Name a task you have for the today!: ")
         tasks.append(task)
+
+        with open("task.txt", "a") as file:
+            for count, item in enumerate(tasks, start=1):
+                file.write(f"{count}. {item}\n")
+
         print("Added to tasks!")
 
     elif choice == 2:
-        for index, item in enumerate(tasks, start=1):
-            print(f"{index}. {item}")
-        print("\n\n")
+        with open("task.txt", "r") as file:
+            taskContent = file.read()
+            print("--- To-Do List ---")
+            print(taskContent)
+
+
+        # for index, item in enumerate(tasks, start=1):
+        #     print(f"{index}. {item}")
+        # print("\n\n")
             
     elif choice == 3:
+
+
+
+
+
+
+
         for index, item in enumerate(tasks, start=1):
             print(f"{index}. {item}")
         tr = input("Which task would you like to remove?: ")
-        if tr not in tasks:
+        if tasks[int(tr)-1] not in tasks:
             print("Not in To-Do List")
         tasks.pop(int(tr)-1)
-        print("Task")
+        print("Task Removed")
 
 
     elif choice == 4:

bc1b808c8342ea5d27b8a8cb116687d31b6b63b8
Rohan Maligi <rmaligi@uci.edu>
2026-09-08 15:50:07 -0700

    added remove function to the code using the file write

--- todo.py
+++ todo.py
@@ -14,7 +14,7 @@ while True:
         task = input("Name a task you have for the today!: ")
         tasks.append(task)
 
-        with open("task.txt", "a") as file:
+        with open("task.txt", "w") as file:
             for count, item in enumerate(tasks, start=1):
                 file.write(f"{count}. {item}\n")
 
@@ -26,26 +26,35 @@ while True:
             print("--- To-Do List ---")
             print(taskContent)
 
+    elif choice == 3:
+
+        removed_task = input("Enter the number of the task that you want to remove: ")
+
+        try:
+            with open("task.txt", "r") as file:
+                lines = file.readlines()
+        except FileNotFoundError:
+            print("The file doesn't exist yet")
+            lines = []
+
+        new_lines = []
+        for line in lines:
+            if removed_task not in line:
+                new_lines.append(line)
+
+        with open("task.txt", "w") as file:
+            file.writelines(new_lines)
+
+        print(f"Removed {removed_task} from the task list.")
+
 
         # for index, item in enumerate(tasks, start=1):
         #     print(f"{index}. {item}")
-        # print("\n\n")
-            
-    elif choice == 3:
-
-
-
-
-
-
-
-        for index, item in enumerate(tasks, start=1):
-            print(f"{index}. {item}")
-        tr = input("Which task would you like to remove?: ")
-        if tasks[int(tr)-1] not in tasks:
-            print("Not in To-Do List")
-        tasks.pop(int(tr)-1)
-        print("Task Removed")
+        # tr = input("Which task would you like to remove?: ")
+        # if tasks[int(tr)-1] not in tasks:
+        #     print("Not in To-Do List")
+        # tasks.pop(int(tr)-1)
+        # print("Task Removed")
 
 
     elif choice == 4:
